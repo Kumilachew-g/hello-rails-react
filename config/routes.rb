dd5be525042261f7cf5171af7753923ea536e5ab
Kumilachew-g <kumilachew1212@gmail.com>
2023-01-03 15:56:14 +0300

    Add root#index in the route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "static_pages#index"
+  root 'root#index'
 
   namespace :api, defaults: { format: :json } do
     resources :greetings, only: [:index]
